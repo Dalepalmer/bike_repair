f64f8689259576ef9ebd0a0714b55d65e104150e
Matthew <meterrill>
2015-04-08 16:41:21 -0700

    add more styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root :to => "home#index"
+
+  resources :test
+  resources :home
 end
